9aa29b90904c8661012d2828b506264c9cbc410b
KaffeChino <KaffeChino@users.noreply.github.com>
2019-04-10 14:04:18 +0800

    添加了注释

--- spider.py
+++ spider.py
@@ -93,11 +93,13 @@ def image_save(data, i, j, zoom):
 
 
 def main():
+
+    # 输入经纬度坐标
     # lat1 = float(input("输入纬度1："))
     # lat2 = float(input("输入纬度2："))
     # lon1 = float(input("输入经度1："))
     # lon2 = float(input("输入经度2："))
-    # zoom = int(input("输入缩放倍率（0-23）："))
+    # zoom = int(input("输入缩放倍率（0-21）："))
 
     lat1 = float(34.967)
     lat2 = float(24.25)
@@ -105,20 +107,28 @@ def main():
     lon2 = float(114.2)
     zoom = int(5)
 
-    lat1, lat2 = switch_big2small(lat1, lat2)
-    lon1, lon2 = switch_big2small(lon1, lon2)
+    lat_samll, lat_large = switch_big2small(lat1, lat2)
+    lon_small, lon_large = switch_big2small(lon1, lon2)
 
-    x_small, y_small = switch_deg_num.deg2num(lat2, lon1, zoom)
-    x_large, y_large = switch_deg_num.deg2num(lat1, lon2, zoom)
+    """
+    调用deg2num函数将经纬度转换成在zoom缩放倍率下瓦块坐标
+    x_small, y_small是该区域左上角瓦块的坐标，经度数小纬度高
+    x_large, y_large是该区域右下角瓦块的坐标，经度数大纬度低
+    """
+    x_small, y_small = switch_deg_num.deg2num(lat_large, lon_small, zoom)
+    x_large, y_large = switch_deg_num.deg2num(lat_samll, lon_large, zoom)
 
     # arglist = [x_small, x_large, y_small, y_large, zoom]
 
+    # 记录爬虫程序开始时间
     start_time = time.perf_counter()
 
     # success, fail = spider_system.run(spider_system, arglist)
+    # 将5个数值传递给爬虫程序开始运行，返回的是成功数和失败数
     success, fail = spider_system.run(
         x_small, x_large, y_small, y_large, zoom)
 
+    # 记录爬虫程序运行结束时间
     end_time = time.perf_counter()
 
     print("Total cost {:.2f} seconds.".format(end_time - start_time))
